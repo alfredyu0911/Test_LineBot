cc61fce4eeed5586a8255d6e2c6e22a7e6486c4d
Alfred <alfredyu0911@gmail.com>
2018-04-12 20:35:11 +0800

    no message

--- app.py
+++ app.py
@@ -38,7 +38,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    if event.message.text == "Hi"
+        message = TextSendMessage("How are you")
+    else
+        message = TextSendMessage("received undefine message")
     line_bot_api.reply_message(
         event.reply_token,
         message)
