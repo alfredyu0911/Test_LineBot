9b9d999a6163f2c130a01f4cceb9e97b03bceb66
Alfred <alfredyu0911@gmail.com>
2018-04-12 23:39:50 +0800

    no message

--- app.py
+++ app.py
@@ -10,6 +10,67 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 )
 
+subscription_key = "f9cbd50a4b6e4696addd5c589279362c"
+assert subscription_key
+face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
+image_url = 'https://how-old.net/Images/faces2/main007.jpg'
+
+import requests
+from IPython.display import HTML
+
+headers = { 'Ocp-Apim-Subscription-Key': subscription_key }
+
+params = {
+    'returnFaceId': 'true',
+    'returnFaceLandmarks': 'false',
+    'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+}
+
+def annotate_image(image_url):
+    response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
+    faces = response.json()
+    
+    image_file = BytesIO(requests.get(image_url).content)
+    image = Image.open(image_file)
+    
+    plt.figure(figsize=(8,8))
+    ax = plt.imshow(image, alpha=0.6)
+    for face in faces:
+        fr = face["faceRectangle"]
+        fa = face["faceAttributes"]
+        origin = (fr["left"], fr["top"])
+        p = patches.Rectangle(origin, fr["width"], \
+                              fr["height"], fill=False, linewidth=2, color='b')
+                              ax.axes.add_patch(p)
+                              plt.text(origin[0], origin[1], "%s, %d"%(fa["gender"].capitalize(), fa["age"]), \
+                                       fontsize=20, weight="bold", va="bottom")
+    plt.axis("off")
+
+response = requests.post(face_api_url, params=params, headers=headers, json={"url": image_url})
+faces = response.json()
+HTML("<font size='5'>Detected <font color='blue'>%d</font> faces in the image</font>"%len(faces))
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+from PIL import Image
+from matplotlib import patches
+from io import BytesIO
+
+response = requests.get(image_url)
+image = Image.open(BytesIO(response.content))
+
+plt.figure(figsize=(8,8))
+ax = plt.imshow(image, alpha=0.6)
+for face in faces:
+    fr = face["faceRectangle"]
+    fa = face["faceAttributes"]
+    origin = (fr["left"], fr["top"])
+    p = patches.Rectangle(origin, fr["width"], fr["height"], fill=False, linewidth=2, color='b')
+    ax.axes.add_patch(p)
+    plt.text(origin[0], origin[1], "%s, %d"%(fa["gender"].capitalize(), fa["age"]), fontsize=20, weight="bold", va="bottom")
+_ = plt.axis("off")
+
 app = Flask(__name__)
 
 # Channel Access Token
@@ -19,6 +80,7 @@ handler = WebhookHandler('e4013cf04ebc446549c202098e9562a8')
 
 @app.route('/')
 def index():
+    annotate_image("https://how-old.net/Images/faces2/main001.jpg")
     return "<p>Hello World!</p>"
 
 # 監聽所有來自 /callback 的 Post Request
